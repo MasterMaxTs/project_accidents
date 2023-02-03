1982f13c8bd75478949adb3988f2749fccda94cd
Maxim Tsurkanov <max86ts@gmail.com>
2023-02-04 00:44:00 +0300

    0. Spring MVC [#6877]. Изменен главный класс приложения App.java на Job4jAccidentsApplication.java

---
+++ src/main/java/ru/job4j/accidents/Job4jAccidentsApplication.java
@@ -0,0 +1,12 @@
+package ru.job4j.accidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jAccidentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jAccidentsApplication.class, args);
+        System.out.println("Go to http://localhost:8080/index");
+    }
+}
